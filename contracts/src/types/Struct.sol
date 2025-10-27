12bf06e8d7dab4ea56c586e3460897d47263ad3f
dimka90 <dimkayilrit@gmail.com>
2025-10-27 05:32:35 +0100

    types: added types for modularity

---
+++ contracts/src/types/Struct.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+struct Contractor {
+    string companyName;
+    uint registrationNumber;
+    uint taxIdenticationNumber;
+    string physicalAddress;
+    address owner;
+    string addressImageCid;
+    string companyUploadedCid;
+}
+
+struct CompletedProjects {
+    uint projectId;
+    string projectDescription;
+    string projectImagecid;
+    bool status;
+}
+
+struct Milestone {
+    uint milestoneId;
+    string description;
+    bool completed;
+    uint paymentAmount;
+    uint dueDate;
+    uint startDate;
+    string milestoneImageCid;
+}
+
+struct RejectedMileStone {
+    uint projectId;
+    uint milestoneId;
+    address contractor;
+}
+struct MileStoneSubMitted {
+    uint projectId;
+    uint milestoneId;
+    address contractor;
+}
+
+struct Project {
+    uint256 projectId;
+    string description;
+    uint256 budget;
+    address contractorAddress;
+    bool completed;
+    uint startDate;
+    uint endDate;
+    string imageCid;
+}
