5dd5eb21aca5c0f9cb6ae394d1efe22f792edd7d
dimka90 <dimkayilrit@gmail.com>
2025-10-31 19:11:40 +0100

    createProject: Added functionality for creating a new project

--- contracts/src/core/Contractor.sol
+++ contracts/src/core/Contractor.sol
@@ -1,7 +1,9 @@
 //SPDX-License-identifier: MIT
 
 pragma solidity ^0.8.20;
-
+import "../interfaces/IERC20.sol";
+import "../types/Enum.sol";
+import "../types/Struct.sol";
 contract Procurement {
     address owner;
     address tokenAddress;
@@ -19,4 +21,47 @@ contract Procurement {
 
     Contractor[] public contractors;
     uint milestoneId;
+
+    function createProject(
+        string memory _description,
+        uint256 _budget,
+        address _contractorAddress,
+        uint _startdate,
+        uint _endate
+    ) external {
+        uint256 allowance = IERC20(tokenAddress).allowance(
+            msg.sender,
+            address(this)
+        );
+        require(
+            _budget <= allowance,
+            "No allowance to spend funds at the moment"
+        );
+        IERC20(tokenAddress).transferFrom(msg.sender, address(this), _budget);
+        // Create a new project for a Contractor
+        Project storage newProject = projects[projectId];
+        newProject.projectId = projectId;
+        newProject.description = _description;
+        newProject.budget = _budget;
+        newProject.contractorAddress = _contractorAddress;
+        newProject.completed = false;
+        newProject.startDate = _startdate;
+        newProject.endDate = _endate;
+        newProject.imageCid = " ";
+
+        //mapp contractors address to the project assigned
+        contractorProjects[_contractorAddress].push(projectId);
+
+        // mapp contractor to agency
+
+        agency_Contractor[msg.sender] = _contractorAddress;
+
+        emit CreateProject(
+            _description,
+            projectId,
+            _contractorAddress,
+            msg.sender
+        );
+        projectId++;
+    }
 }
