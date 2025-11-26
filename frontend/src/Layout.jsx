0ed4b963da23207f84f12545fa7a7fdea9fe4014
dimka90 <dimkayilrit@gmail.com>
2025-11-26 22:56:56 +0100

    setup: Layout Jsx

---
+++ frontend/src/Layout.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import SideMenu from "./components/SideMenu";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import AgencySideMenu from "./components/AgencySideMenu";
+import Home from "./pages/Home";
+
+function Layout() {
+  return (
+    <div className="grid grid-rows-layout grid-cols-[250px_1fr] n">
+    
+      <header className="row-span-1 col-span-full text-white">
+        <Navbar />
+      </header>
+
+    
+      {/* <aside className="row-span-2 col-span-1  text-white">
+        <AgencySideMenu />
+      </aside> */}
+      <aside className="row-span-2 col-span-1  text-white mt-20">
+        <SideMenu />
+      </aside>
+
+      <main className="row-span-2 col-span-1 p-4 overflow-y-auto mt-8">
+        <Outlet />
+      </main>
+      <footer className="row-span-1 col-span-full border ">
+        {/* <Footer/> */}
+      </footer>
+    </div>
+  );
+}
+
+export default Layout;
