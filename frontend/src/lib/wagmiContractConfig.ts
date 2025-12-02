f4902c2d1ecdf4288fae70151c474d134ad210d7
dimka90 <dimkayilrit@gmail.com>
2025-12-02 13:00:35 +0100

    added lib folder

---
+++ frontend/src/lib/wagmiContractConfig.ts
@@ -0,0 +1,18 @@
+import { WagmiAbi } from "../wagmiAbi";
+
+const wagmiContractConfig = {
+  // address: "0xabDe84F6FfC9f44C978bD9dA76319316EeeA01BB",
+  // address: "0xc48cb09fdddf80e04476c1ecc393da5706da8501",
+  // address: "0xf5d1863c6b954c407090b48dbaf60f24f9a8b213",
+  // address: "0x6921D24Ac3F409a2C4204e4c15167D8570B5DB18",
+  // address:"0x559E854725884F633E680d62A159C94e79BEF657",
+  // address:"0x3c5bff88446caf1a95676c50412347b1884b59d6"
+  // address: "0x799e9fdaa0f0e4ac6542ce2259025bf9d1de98dc",
+  // address: "0x347272e87068eA428E5d77CdC479789D0f033671",
+  // address: "0xa1f967b639832E32BF462E0084A8dbc0E178005d",
+  // address: "0x54e766DB13D1EEe7a3a7bb70808df656Be051577",
+   address: "0xfd803493a89f96f72f48b25aec4d557bbf95e41d",
+  abi: WagmiAbi,
+};
+
+export { wagmiContractConfig };
